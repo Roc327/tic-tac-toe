8072c7286b28c5f97d31335e464edaecf19707db
Nate <nate_van@hotmail.com>
2024-02-20 12:59:35 -0500

    added new functions for testing
    Feat: function to add players to array
    Feat: function to start game flow
    Both basic for testing in console.

--- js/ticTacToe.js
+++ js/ticTacToe.js
@@ -13,6 +13,23 @@ const player = function (name) {
   return { playerName, getScore, givePoint };
 };
 
+function addPlayers() {
+  // some temp code to test game in console
+  const players = [];
+  players.push(player(prompt("Enter name for Player 1:")));
+  if (prompt("Would you like to enter another player? Type y/n:") == "y") {
+    players.push(player(prompt("What is the players name? ")));
+  } else {
+    players.push(player("CPU"));
+  }
+  return players;
+}
+
 const gameFlow = function () {
   // code for game flow factory function
+  const playerList = addPlayers();
+  console.log(
+    `We have ${playerList[0].playerName()} playing against ${playerList[1].playerName()} in a glorious battle of....`,
+  );
+  console.log("TIC TAC TOE!!!");
 };
