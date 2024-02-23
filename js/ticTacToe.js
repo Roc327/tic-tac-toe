6e7636b411b87b765692c6d21c9b22052cd52845
Nate <nate_van@hotmail.com>
2024-02-23 02:44:46 -0500

    started on a gameboard function needs to be updated to add player mark

--- js/ticTacToe.js
+++ js/ticTacToe.js
@@ -1,6 +1,42 @@
-const gameBoard = (function () {
-  // code for gameboard factory function
-})();
+const gameBoard = function () {
+  // code for gameboard factory function'
+  const rows = 3;
+  const columns = 3;
+  const board = [];
+
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push(Cell());
+    }
+  }
+
+  const getBoard = () => board;
+
+  const placeMark = (column, row, player) => {
+    const availableCells = board
+      .filter((row) => row[column].getValue() === 0)
+      .map((row) => row[column]);
+
+    /* 
+      * The following code needs to be updated, this was from a connect four game so mark drops to lowest row *
+    
+    if (!availableCells.length) return;
+
+    const lowestRow = availableCells.length - 1;
+    board[lowestRow][column].addMark(player);
+    */
+  };
+
+  const printBoard = () => {
+    const boardWithCellValues = board.map((row) =>
+      row.map((cell) => cell.getValue()),
+    );
+    console.log(boardWithCellValues);
+  };
+
+  return { getBoard, placeMark, printBoard };
+};
 
 const player = function (name) {
   this.name = name;
